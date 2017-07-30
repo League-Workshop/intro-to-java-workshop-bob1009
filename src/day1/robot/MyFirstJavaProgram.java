8db63a8393ae129e073af2efd242dcb5e2abdf0b
samus114 <league@WTS15.attlocal.net>
2017-07-30 14:28:30 -0700

    Day 1

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
-	}
+Robot r2d2 = new Robot();
+r2d2.penDown();
+r2d2.sparkle();
+;
+for (int i = 0; i <16; i++) {
+	r2d2.move(100);	
+	r2d2.turn(422);	
 }
+
+
+	}
+	}
+
